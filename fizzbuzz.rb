2716ee3a409c3e1e63f4a2c7189aad4010e8e875
mruly <mruly@users.noreply.github.com>
2015-12-15 19:10:02 +0400

    loop moved out of fizzbuzz(). used ternary operator in fizzbuzz().

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,10 @@
-def fizzbuzz
-  (1..100).each do |n|
-    output = ""
-    output << "Fizz" if n % 3 == 0
-    output << "Buzz" if n % 5 == 0
-    output = n if output.empty?
-    puts output
-  end
+def fizzbuzz(n)
+  output = ""
+  output << "Fizz" if n % 3 == 0
+  output << "Buzz" if n % 5 == 0
+  output.empty? ? n : output
 end
 
-fizzbuzz
+(1..100).each do |i|
+  puts fizzbuzz(i)
+end
