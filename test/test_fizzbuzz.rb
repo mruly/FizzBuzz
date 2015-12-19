344df45aa245d8f80b4d0e8456a92ea1aa315e85
mruly <mruly@users.noreply.github.com>
2015-12-19 13:49:35 +0400

    fixed problem with tests with random numbers

--- test/test_fizzbuzz.rb
+++ test/test_fizzbuzz.rb
@@ -4,11 +4,13 @@ require_relative '../fizzbuzz'
 class TestFizzBuzz < MiniTest::Unit::TestCase
   def test_fizz
     assert_equal "Fizz", fizzbuzz(3)
-    assert_equal "Fizz", fizzbuzz(3 * rand(10))
+    n = rand(10)
+    assert_equal "Fizz", fizzbuzz(3 * n) unless n%5 == 0
   end
   def test_buzz
     assert_equal "Buzz", fizzbuzz(5)
-    assert_equal "Buzz", fizzbuzz(5 * rand(10))
+    n = rand(10)
+    assert_equal "Buzz", fizzbuzz(5 * n) unless n%3 == 0
   end
   def test_fizzbuzz
     assert_equal "FizzBuzz", fizzbuzz(15)
@@ -17,6 +19,7 @@ class TestFizzBuzz < MiniTest::Unit::TestCase
   def test_integer
     assert_equal 1, fizzbuzz(1)
     assert_equal 2, fizzbuzz(2)
+    assert_equal 49, fizzbuzz(49)
     assert_equal 98, fizzbuzz(98)
   end
 end
